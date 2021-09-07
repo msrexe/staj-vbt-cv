a571b37d1f89075499738cb55c864d72db41e9a2
Melih Sivri <mlheymen.ms@gmail.com>
2021-09-07 14:46:35 +0300

    margin fix

--- index.js
+++ index.js
@@ -1,60 +1,80 @@
 function getPositions() {
-        //api işlemleri
-    return ["IK", "Satış", "Java Developer", "C# Developer"]
+  //api işlemleri
+  return ["IK", "Satış", "Java Developer", "C# Developer"];
 }
 
 function getTags() {
-    //api işlemleri
-    return ["C#", "Go", "Python", "Java", "JavaScript", "Docker", "Kubernetes", "React", "Angular", "TypeScript", "Vue", "Rust", "Elixir"]
+  //api işlemleri
+  return [
+    "C#",
+    "Go",
+    "Python",
+    "Java",
+    "JavaScript",
+    "Docker",
+    "Kubernetes",
+    "React",
+    "Angular",
+    "TypeScript",
+    "Vue",
+    "Rust",
+    "Elixir",
+  ];
 }
 
-
 function loadTags() {
-    document.getElementById("tags").innerHTML = ''
+  document.getElementById("tags").innerHTML = "";
 
-    let tagsArr = getTags()
-    tagsArr.forEach(element => {
-        document.getElementById("tags").innerHTML += '<li><div><input type="checkbox" class="checkbox-round" id="'+element+'" /><label for="'+element+'">'+element+'</label></div></li>'
-    });
-
+  let tagsArr = getTags();
+  tagsArr.forEach((element) => {
+    document.getElementById("tags").innerHTML +=
+      '<li><div class="tag"><input type="checkbox" class="checkbox-round" id="' +
+      element +
+      '" /><label for="' +
+      element +
+      '" >' +
+      element +
+      "</label></div></li>";
+  });
 }
 
 function loadPositions() {
-    document.getElementById("position-combobox").innerHTML = '<option value="" disabled selected>Pozisyon Seç</option>'
+  document.getElementById("position-combobox").innerHTML =
+    '<option value="" disabled selected>Pozisyon Seç</option>';
 
-    let positionsArr = getPositions()
-    positionsArr.forEach(element => {
-        document.getElementById("position-combobox").innerHTML += '<option value="'+element+'">'+element+'</option>'
-    });
+  let positionsArr = getPositions();
+  positionsArr.forEach((element) => {
+    document.getElementById("position-combobox").innerHTML +=
+      '<option value="' + element + '">' + element + "</option>";
+  });
 }
 
 function getSelectedTags() {
-    let tagIds = getTags()
-    let selectedTags = []
-    tagIds.forEach(element => {
-        if (document.getElementById(element).checked) {
-            selectedTags.push(element)
-        }
-    });
-    console.log(selectedTags);
-    return selectedTags
+  let tagIds = getTags();
+  let selectedTags = [];
+  tagIds.forEach((element) => {
+    if (document.getElementById(element).checked) {
+      selectedTags.push(element);
+    }
+  });
+  console.log(selectedTags);
+  return selectedTags;
 }
 
 function getSelectedPosition() {
-    let dropdown = document.getElementById("position-combobox")
-    return dropdown.value
+  let dropdown = document.getElementById("position-combobox");
+  return dropdown.value;
 }
 
 function getUploadedFiles() {
-
+  //drag işlemleri
 }
 
 function submitValues() {
-    console.log(getSelectedPosition())
-    console.log(getSelectedTags())
+  console.log(getSelectedPosition());
+  console.log(getSelectedTags());
 }
 
-
 //on init
-loadPositions()
-loadTags()
+loadPositions();
+loadTags();
